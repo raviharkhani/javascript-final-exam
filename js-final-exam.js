bc2e1d4048c37f2469215e0b8413ec522dc1c6cd
Ravi Harkhani <128570370+raviharkhani@users.noreply.github.com>
2023-06-28 21:45:45 +0530

    Update js-final-exam.js

--- js-final-exam.js
+++ js-final-exam.js
@@ -1,13 +1,22 @@
-function add(){
-    let box = document.getElementById("ravi");
+function btnclik() {
+            var inpval = Number(document.getElementById("input1").value);
+            var panelty1 = document.getElementById("jar");
+            panelty1.innerText = inpval;
 
-    let inputValue = document.getElementById("input-value");
 
-    if (inputValue.value != 0) {
-        box.innerHTML = parseInt(box.innerHTML) + parseInt(inputValue.value); 
-        document.getElementById("input-value").value = "";
+        };
+        document.getElementById("btn").addEventListener("click", btnclik);
 
-    }else{
-    box.innerHTML = parseInt(box.innerHTML)  + 0;
-    }          
-}
+        function addvalue() {
+
+            var inpval = Number(document.getElementById("input1").value);
+            var panelty1 = document.getElementById("jar");
+            panelty1.innerText = inpval;
+
+            var r = document.getElementById("h1");
+            r.innerText = inpval;
+            r.innerText = Number(r.innerText) + inpval;
+        }
+
+        document.getElementById("btn2").addEventListener("click", addvalue);
+
